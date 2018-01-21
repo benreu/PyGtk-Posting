1d14ae08eab9ade863e541bbf0e9be08d66dd071
Reuben Rissler <pygtk.posting@gmail.com>
2018-01-20 19:14:36 -0500

    better search for job sheet history

--- src/reports/job_sheet_history.py
+++ src/reports/job_sheet_history.py
@@ -31,62 +31,43 @@ class JobSheetHistoryGUI:
 
 		self.db = db
 		self.cursor = self.db.cursor()
-
-		self.customer_id = None
-		self.job_sheet_treeview = self.builder.get_object('treeview1')
+		self.customer_text = ''
+		self.ext_name = ''
+		self.job_text = ''
+		self.description_text = ''
 		
-		self.customer_store = self.builder.get_object('customer_store')
-		customer_completion = self.builder.get_object('customer_completion')
-		customer_completion.set_match_func(self.customer_match_func)
 		self.job_sheet_store = self.builder.get_object('job_store')
-		self.job_sheet_line_item_store = self.builder.get_object('liststore2')
-		self.search_store = Gtk.ListStore(str)
-		
-		self.populate_customers()
+		self.job_sheet_line_item_store = self.builder.get_object('job_line_item_store')
+		self.job_filter = self.builder.get_object('job_filter')
+		self.job_filter.set_visible_func(self.filter_func)
+
+		self.populate_job_sheet_treeview ()
 		
 		self.window = self.builder.get_object('window1')
 		self.window.show_all()
 
-	def search_changed(self, widget):
-		search_text = widget.get_text()	
-		self.search_store.clear()
-		treeiter = self.job_sheet_store.get_iter_first()
-		while treeiter != None:
-			for cell in self.job_sheet_store[treeiter][:]:
-				if search_text in str(cell):
-					cursor_iter = self.job_sheet_store.get_string_from_iter(treeiter)
-					self.search_store.append([cursor_iter])
-			treeiter = self.job_sheet_store.iter_next(treeiter)
+	def filter_func(self, model, tree_iter, r):
+		for text in self.customer_text.split():
+			if text not in model[tree_iter][1].lower():
+				return False
+		for text in self.ext_name.split():
+			if text not in model[tree_iter][2].lower():
+				return False
+		for text in self.job_text.split():
+			if text not in model[tree_iter][5].lower():
+				return False
+		for text in self.description_text.split():
+			if text not in model[tree_iter][6].lower():
+				return False
+		return True
 
-	def search_forward(self, widget):
-		c = self.job_sheet_treeview.get_column(0)
-		try:
-			self.search_iter += 1
-			row = self.search_store[self.search_iter]
-		except: # tumble over to the start
-			self.search_iter = 0
-			row = ["1000000"] # a number that is not valid so no row is selected
-			for i in self.search_store: 
-				row = self.search_store[self.search_iter]
-				break # we have a valid row now
-		path = Gtk.TreePath.new_from_string(row[0])
-		self.job_sheet_treeview.expand_to_path(path)
-		self.job_sheet_treeview.set_cursor(row , c, True)
-
-	def search_backward(self, widget):
-		c = self.job_sheet_treeview.get_column(0)
-		try:
-			self.search_iter -= 1
-			row = self.search_store[self.search_iter]
-		except: # tumble over to the end
-			self.search_iter = len(self.search_store) - 1
-			row = ["1000000"] # a number that is not valid
-			for i in self.search_store: 
-				row = self.search_store[self.search_iter]
-				break # we have a valid row now
-		path = Gtk.TreePath.new_from_string(row[0])
-		self.job_sheet_treeview.expand_to_path(path)
-		self.job_sheet_treeview.set_cursor(row , c, True)
+	def search_changed (self, entry):
+		"activated by any of the search entries changing"
+		self.customer_text = self.builder.get_object("searchentry1").get_text()
+		self.ext_name = self.builder.get_object("searchentry4").get_text()
+		self.job_text = self.builder.get_object("searchentry2").get_text()
+		self.description_text = self.builder.get_object("searchentry3").get_text()
+		self.job_filter.refilter()
 
 	def remark_edited(self, widget, path, text):
 		_id_ = self.job_sheet_line_item_store[path][0]
@@ -96,6 +77,7 @@ class JobSheetHistoryGUI:
 		self.db.commit()
 
 	def focus (self, window, event):
+		return
 		tree_selection = self.builder.get_object('treeview-selection1')
 		model, path = tree_selection.get_selected_rows()
 		self.populate_job_sheet_treeview()
@@ -106,45 +88,25 @@ class JobSheetHistoryGUI:
 		job_sheet_id = model[path][0]
 		self.populate_job_sheet_line_item_treeview(job_sheet_id)
 
-	def customer_changed (self, widget):
-		customer_id = widget.get_active_id()
-		if customer_id != None:
-			self.customer_id = customer_id
-			self.populate_job_sheet_treeview()
-
-	def customer_match_selected (self, completion, model, iter_):
-		self.customer_id = model[iter_][0]
-		self.populate_job_sheet_treeview()
-
 	def populate_job_sheet_treeview (self):
 		self.job_sheet_store.clear()
 		self.job_sheet_line_item_store.clear()
-		view_checkbutton = self.builder.get_object('checkbutton1')
-		if view_checkbutton.get_active() == True:
-			self.cursor.execute("SELECT id, contact_id, job_type_id, "
-								"date_inserted, description FROM job_sheets")
-		elif self.customer_id != None:
-			self.cursor.execute("SELECT id, contact_id, job_type_id, "
-								"date_inserted, description "
-								"FROM job_sheets WHERE contact_id = %s", 
-								(self.customer_id,))
-		else:
-			return #no active customer and 'view all' not on
+		self.cursor.execute("SELECT js.id, c.name, c.ext_name, jt.name, "
+								"date_inserted, description FROM job_sheets AS js "
+								"JOIN contacts AS c ON c.id = contact_id "
+								"JOIN job_types AS jt ON jt.id = job_type_id")
 		for row in self.cursor.fetchall():
 			row_id = row[0]
-			contact_id = row[1]
-			job_type_id = row[2]
-			self.cursor.execute("SELECT name FROM job_types "
-								"WHERE id = %s", (job_type_id,))
-			job_type_name = self.cursor.fetchone()[0]
-			date = row[3]
+			contact_name = row[1]
+			contact_ext_name = row[2]
+			job_type_name = row[3]
+			date = row[4]
 			date_text = datetime_to_text(date)
-			description = row[4]
-			self.cursor.execute("SELECT name, c_o FROM contacts "
-								"WHERE id = %s", (contact_id,))
-			contact_name = self.cursor.fetchone()[0]
-			self.job_sheet_store.append([row_id, contact_name, str(date), 
+			description = row[5]
+			self.job_sheet_store.append([row_id, contact_name, contact_ext_name, str(date), 
 										date_text, job_type_name, description])
+			while Gtk.events_pending():
+				Gtk.main_iteration()
 
 	def job_sheet_treeview_activate(self, treeview, path, treeviewcolumn):
 		job_sheet_id = self.job_sheet_store[path][0]
@@ -165,34 +127,6 @@ class JobSheetHistoryGUI:
 			self.job_sheet_line_item_store.append([row_id, qty, 
 													product_name, 
 													remark])
-		
-	def populate_customers (self):		
-		self.cursor.execute("SELECT contacts.id, contacts.name, contacts.c_o "
-							"FROM job_sheets "
-							"JOIN contacts "
-							"ON contacts.id = job_sheets.contact_id "
-							"GROUP BY contacts.id "
-							"ORDER BY contacts.name")
-		for row in self.cursor.fetchall():
-			customer_id = row[0]
-			customer_name = row[1]
-			customer_co = row[2]
-			self.customer_store.append([str(customer_id), 
-										customer_name, 
-										customer_co])
-
-	def view_button_toggled (self, widget):
-		if widget.get_active() == False:
-			widget.set_label("View all")
-		else:
-			widget.set_label("View selected")
-		self.populate_job_sheet_treeview()
-
-	def customer_match_func(self, completion, key, _iter):
-		for text in key.split():
-			if text not in self.customer_store[_iter][1].lower():
-				return False
-		return True
 
 
 
