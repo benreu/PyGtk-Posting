6284f5fbb40abd93a96be2915d4db2ce8e4b9a70
Reuben Rissler <pygtk.posting@gmail.com>
2018-05-14 06:40:35 -0400

    change incoming invoices window to use external pdf attachment module

--- src/incoming_invoice.py
+++ src/incoming_invoice.py
@@ -16,10 +16,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import Gtk, GLib, GObject
-from multiprocessing import Queue, Process
-from queue import Empty
-from subprocess import Popen, PIPE, STDOUT, call
-import re, sane, os
+import re
 from decimal import Decimal, ROUND_HALF_UP
 from dateutils import DateTimeCalendar
 from check_writing import set_written_ck_amnt_text, get_check_number
@@ -64,12 +61,8 @@ class IncomingInvoiceGUI:
 	
 		self.window = self.builder.get_object('window1')
 		self.window.show_all()
-		self.data_queue = Queue()
-		self.scanner_store = self.builder.get_object("scanner_store")
+		
 		self.file_data = None
-		thread = Process(target=self.get_scanners)
-		thread.start()
-		GLib.timeout_add(100, self.populate_scanners)
 
 	def amount_focus_in_event (self, entry, event):
 		GLib.idle_add(self.highlight, entry)
@@ -378,108 +371,12 @@ class IncomingInvoiceGUI:
 		self.calendar.set_relative_to(widget)
 		self.calendar.show()
 
-	def populate_scanners(self):
-		try:
-			devices = self.data_queue.get_nowait()
-			for scanner in devices:
-				device_id = scanner[0]
-				device_manufacturer = scanner[1]
-				name = scanner[2]
-				given_name = scanner[3]
-				self.scanner_store.append([str(device_id), device_manufacturer,
-											name, given_name])
-		except Empty:
-			return True
-		
-	def get_scanners(self):
-		sane.init()
-		devices = sane.get_devices()
-		self.data_queue.put(devices)
-
 	def attach_button_clicked (self, button):
-		dialog = self.builder.get_object('scan_dialog')
-		self.builder.get_object('button10').set_sensitive (False)
-		combo = self.builder.get_object('combobox2')
-		combo.set_active(-1)
-		combo.set_sensitive(True)
-		chooser = self.builder.get_object('filechooserbutton1')
-		chooser.unselect_all()
-		chooser.set_sensitive(True)
+		import pdf_attachment
+		dialog = pdf_attachment.Dialog(self.window)
 		result = dialog.run()
-		self.builder.get_object('pdf_opt_result_buffer').set_text('', -1)
 		if result == Gtk.ResponseType.ACCEPT:
-			if self.attach_origin == 1:
-				self.scan_file()
-			elif self.attach_origin == 2:
-				self.attach_file_from_disk()
-			elif self.attach_origin == 3:
-				call(["cp", self.file_name, "/tmp/opt.pdf"])
-				self.attach_file_from_disk()
-			self.builder.get_object('button10').set_label('Attach')
-			self.builder.get_object('label20').set_visible(False)
-		dialog.hide()
-
-	def scanner_combo_changed (self, combo):
-		self.builder.get_object('filechooserbutton1').set_sensitive (False)
-		self.attach_origin = 1
-		self.builder.get_object('button8').set_sensitive (True)
-
-	def filechooser_file_set (self, chooser):
-		if os.path.exists("/tmp/opt.pdf"):
-			os.remove("/tmp/opt.pdf")
-		self.spinner = self.builder.get_object('spinner1')
-		self.spinner.start()
-		self.spinner.set_visible(True)
-		self.result_buffer = self.builder.get_object('pdf_opt_result_buffer')
-		self.result_buffer.set_text('', -1)
-		self.sw = self.builder.get_object('scrolledwindow3')
-		self.file_name = chooser.get_filename()
-		p = Popen(['./src/pdf_opt/pdfsizeopt', self.file_name, '/tmp/opt.pdf'], 
-														stdout = PIPE,
-														stderr = STDOUT,
-														stdin = PIPE,
-														close_fds = False)
-		self.io_id = GObject.io_add_watch(p.stdout, GObject.IO_IN, self.optimizer_thread)
-		GObject.io_add_watch(p.stdout, GObject.IO_HUP, self.thread_finished)
-
-	def thread_finished (self, stdout, condition):
-		stdout.close()
-		self.spinner.stop()
-		self.spinner.set_visible(False)
-		GObject.source_remove(self.io_id)
-		button = self.builder.get_object('button10')
-		button.set_sensitive(True)
-		if os.path.exists("/tmp/opt.pdf"):
-			self.builder.get_object('combobox5').set_sensitive (False)
-			button.set_label("Attach")
-			self.attach_origin = 2
-		else:
-			label = self.builder.get_object('label20')
-			label.set_visible(True)
-			label.show()
-			button.set_label("Attach without optimization")
-			self.attach_origin = 3
-
-	def optimizer_thread (self, stdout, condition):
-		line = stdout.readline()
-		line = line.decode(encoding="utf-8", errors="strict")
-		adj = self.sw.get_vadjustment()
-		adj.set_value(adj.get_upper() - adj.get_page_size())
-		iter_ = self.result_buffer.get_end_iter()
-		self.result_buffer.insert(iter_, line, -1)
-		return True
-
-	def scan_file (self):
-		device_address = self.builder.get_object("combobox5").get_active_id()
-		device = sane.open(device_address)
-		document = device.scan()
-		document.save("/tmp/opt.pdf")
-		self.attach_file_from_disk ()
-
-	def attach_file_from_disk (self):
-		f = open("/tmp/opt.pdf",'rb')
-		self.file_data = f.read ()
-		f.close()
+			self.file_data = dialog.get_pdf ()
 		
 
 		
