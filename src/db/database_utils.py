ea9d4ef2a5b2b89eb657c5a55381ecd32d86e3f8
Reuben Rissler <pygtk.posting@gmail.com>
2018-08-02 16:04:44 -0400

    update progressbar to 126

--- src/db/database_utils.py
+++ src/db/database_utils.py
@@ -127,7 +127,7 @@ def check_and_update_version (db, statusbar):
 	cursor = db.cursor()
 	cursor.execute("SELECT version FROM settings")
 	version = cursor.fetchone()[0]
-	COMPLETE_PROGRESS = 107.00
+	COMPLETE_PROGRESS = 126.00
 	progressbar (1)
 	if version <= "016":
 		progressbar (16)
