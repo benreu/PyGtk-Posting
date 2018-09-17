5426df574b0a4b1ead145e3d3f3fdc5a4d58c035
Reuben Rissler <pygtk.posting@gmail.com>
2018-09-17 13:29:10 -0400

    credit_memo_template.py was missing Gtk import

--- src/credit_memo_template.py
+++ src/credit_memo_template.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from gi.repository import Gtk
 import re, subprocess
 from db import transactor
 import printing
