83376ff74fe1ba0e773c792e1a878251f890c43f
Reuben Rissler <pygtk.posting@gmail.com>
2018-09-21 21:13:55 -0400

    fix syntax error in invoiced.py to fix debian packaging errors

--- src/inventory/invoiced.py
+++ src/inventory/invoiced.py
@@ -1,13 +1,12 @@
-# -*- Mode: Python; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- #
 # invoiced.py
-# Copyright (C) 2016 reuben <reuben@reuben-programmer>
+# Copyright (C) 2016 reuben >
 #
-# pygtk_posting-0.1.4 is free software: you can redistribute it and/or modify it
+# invoiced.py is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# pygtk_posting-0.1.4 is distributed in the hope that it will be useful, but
+# invoiced.py is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
@@ -17,6 +16,6 @@
 
 
 def invoiced(store, location_id):
-	
+	pass
 
 
