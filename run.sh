73cd62a3dcf174988ef4bf8b76fbc4648a7c5590
Reuben Rissler <pygtk.posting@gmail.com>
2018-10-06 20:20:50 -0400

    check for logs directory separately from config directory on startup

--- run.sh
+++ run.sh
@@ -3,12 +3,15 @@
 config_dir=$HOME"/.config/posting/"
 if [ ! -d $config_dir ]; then
 	mkdir $config_dir
-	mkdir $config_dir"logs/"
+fi
+log_dir=$HOME"/.config/posting/logs"
+if [ ! -d $log_dir ]; then
+	mkdir $log_dir
 fi
 RELATIVEDIR=`echo $0|sed s/run.sh//g`
 cd $RELATIVEDIR
 
-LOG_FILE=$config_dir"logs/`echo $0|date +%Y-%m-%d:%H:%M:%S`"
+LOG_FILE=$log_dir`echo $0|date +%Y-%m-%d:%H:%M:%S`
 echo "log file is" $LOG_FILE
 chmod +x ./src/pygtk_posting.py &> "$LOG_FILE"
 python3 -u ./src/pygtk_posting.py "$LOG_FILE" &>> "$LOG_FILE"
