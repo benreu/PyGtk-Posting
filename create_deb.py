b74111908ffe6a095a9236b6809cc5729c90400d
Reuben Rissler <pygtk.posting@gmail.com>
2018-10-09 22:25:43 -0400

    add code comments in create_deb.py

--- create_deb.py
+++ create_deb.py
@@ -23,6 +23,7 @@ CWD = os.getcwd()
 
 
 def copy_files (folder, dest_folder):
+	"copy all the .py, .sql, and .ui files to their respective folders"
 	orig_folder = JOIN(CWD, folder)
 	with os.scandir(orig_folder) as objects:
 		for obj in (o for o in objects if o.name != "__pycache__"):
@@ -51,28 +52,34 @@ def copy_files (folder, dest_folder):
 				os.chmod (py_dest, 0o644)
 
 with open ("./Makefile", 'r') as mf:
+	"read Anjuta makefile for version number"
 	for line in mf.read().split('\n'):
 		if line[0:14] == 'PACKAGE_STRING': # get current version of Posting
 			tupl = line.split()
 			version = tupl[-1]
 output = ''
 with open ("control", 'r') as old_c:
+	"read current debian control file"
 	for row, text in enumerate(old_c.read().split('\n')):
 		if row == 1:
 			output += ("Version: %s\n" % version)
 		elif text != '':
 			output += (text + "\n")
 with open ("control", 'w') as new_c:
+	"write posting version to debian control file"
 	new_c.write(output)
 package_name = "pygtk_posting_%s-1" % version
 package_folder = JOIN(CWD, package_name)
 if os.path.exists(package_folder):
-	print ("folder %s already exists, please remove before generating package" % package_folder)
+	print ("folder %s already exists, this may result in improper debian packaging" % package_folder)
+#create .ui file directory
 ui_dest_folder = JOIN(package_folder, "usr/share/pygtk_posting/ui")
 os.makedirs (ui_dest_folder)
+#create .py file directory
 py_dest_folder = JOIN(package_folder, "usr/lib/python3/dist-packages/pygtk_posting")
 os.makedirs (py_dest_folder)
 copy_files ("src", '')
+#create .odt template folders
 odt_dest_folder = JOIN(package_folder, "usr/share/pygtk_posting/templates")
 os.makedirs (odt_dest_folder)
 with os.scandir(JOIN(CWD, "templates")) as odts:
@@ -80,31 +87,40 @@ with os.scandir(JOIN(CWD, "templates")) as odts:
 		end = odt.name.endswith
 		if end(".odt") or end(".txt") or end(".pil") or end(".pbm"): 
 			shutil.copy2(odt, odt_dest_folder)
+#create .page help file directory
 help_dest_folder = JOIN(package_folder, "usr/share/help/C/pygtk-posting")
 os.makedirs (help_dest_folder)
 with os.scandir(JOIN(CWD, "help/C/pygtk-posting")) as pages:
 	for page in pages:
 		if page.name.endswith(".page") : 
 			shutil.copy2(page, help_dest_folder)
+#create usr/bin folder + main executable
 exec_dest_folder = JOIN(package_folder, "usr/bin")
 os.makedirs (exec_dest_folder)
 shutil.copy2(JOIN(CWD, "pygtk-posting"), exec_dest_folder)
+#create desktop entry
 desktop_dest_folder = JOIN(package_folder, "usr/share/applications")
 os.makedirs (desktop_dest_folder)
 shutil.copy2(JOIN(CWD, "pygtk-posting.desktop"), desktop_dest_folder)
+#create icon directory
 shutil.copytree(JOIN(CWD, "icons"), JOIN(package_folder, "usr/share/icons"))
+#create directory required for all debian packages
 doc_dest_folder = JOIN(package_folder, "usr/share/doc/pygtk-posting")
 os.makedirs (doc_dest_folder)
 shutil.copy2(JOIN(CWD, "copyright"), doc_dest_folder)
+#include code128 truetype font for templates with barcodes
 font_dest_folder = JOIN(package_folder, "usr/share/fonts/truetype/code128")
 os.makedirs (font_dest_folder)
 shutil.copy2(JOIN(CWD, "code128.ttf"), font_dest_folder)
+#copy files for debian packaging
 debian_folder = JOIN(package_folder, "DEBIAN")
 os.mkdir (debian_folder)
 shutil.copy2(JOIN(CWD, "control"), debian_folder)
 shutil.copy2(JOIN(CWD, "postinst"), debian_folder)
 shutil.copy2(JOIN(CWD, "prerm"), debian_folder)
+#create debian package
 subprocess.call(["dpkg-deb", "--build", package_name])
+#call lintian to report problems
 subprocess.call(["lintian", package_name+".deb"])
 shutil.rmtree (package_folder)
 
