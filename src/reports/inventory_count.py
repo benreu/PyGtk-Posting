859b6ab58124ad9af006fdeda111a5329861a2d1
Reuben Rissler <pygtk.posting@gmail.com>
2018-12-07 13:23:34 -0500

    add product selection combo and qty editor to inventory count

--- src/reports/inventory_count.py
+++ src/reports/inventory_count.py
@@ -18,6 +18,7 @@
 
 
 from gi.repository import Gtk
+from decimal import Decimal
 from pricing import product_retail_price
 
 UI_FILE = "src/reports/inventory_count.ui"
@@ -32,14 +33,42 @@ class InventoryCountGUI:
 
 		self.db = db
 		self.cursor = db.cursor()
-		self.product_count = 0
-		self.retail = 0.00
-		self.cost = 0.00
 		self.inventory_store = self.builder.get_object('inventory_store')
+		self.product_store = self.builder.get_object('product_store')
+		self.cursor.execute("SELECT id::text, name || ' {' || ext_name || '}' "
+							"FROM products ORDER BY name, ext_name")
+		for row in self.cursor.fetchall():
+			self.product_store.append(row)
+		product_completion = self.builder.get_object('product_completion')
+		product_completion.set_match_func(self.product_match_func)
 
 		self.window = self.builder.get_object('window1')
 		self.window.show_all()
+	
+	def product_match_func(self, completion, key, tree_iter):
+		split_search_text = key.split()
+		for text in split_search_text:
+			if text not in self.product_store[tree_iter][1].lower():
+				return False
+		return True
 
+	def product_combo_changed (self, combobox):
+		product_id = combobox.get_active_id()
+		if product_id == None:
+			return
+		self.cursor.execute("SELECT name, cost, barcode FROM products "
+							"WHERE id = %s", (product_id,))
+		for row in self.cursor.fetchall():
+			self.process_product(int(product_id), row[0], row[1], row[2])
+			self.calculate_totals ()
+
+	def product_match_selected (self, completion, treemodel, treeiter):
+		product_id = treemodel[treeiter][0]
+		self.builder.get_object('product_combo').set_active_id(product_id)
+
+	def qty_edited (self, cellrenderertext, path, qty):
+		self.inventory_store[path][2] = int(qty)
+		self.calculate_totals()
 
 	def barcode_entry_activated (self, entry):
 		barcode = entry.get_text()
@@ -49,29 +78,34 @@ class InventoryCountGUI:
 		self.cursor.execute("SELECT id, name, cost FROM products "
 							"WHERE barcode = %s", (barcode,))
 		for row in self.cursor.fetchall():
-			product_id = row[0]
-			product_name = row[1]
-			product_cost = float(row[2])
-			break
-		else:
-			return # no product
-		self.product_count += 1
+			self.process_product(row[0], row[1], row[2], barcode)
+			self.calculate_totals ()
+
+	def process_product(self, product_id, product_name, product_cost, barcode, qty = 0):
 		for row in self.inventory_store:
 			if row[0] == product_id:
-				row[2] += 1
-				self.cost += row[4]
-				self.retail += row[5]
+				if qty != 0:
+					row[2] = qty 
+				else:
+					row[2] += 1
 				break
 			continue
 		else:
 			retail = product_retail_price (self.db, product_id)
-			self.retail += retail
-			self.cost += product_cost
 			self.inventory_store.append([product_id, barcode, 1, product_name,
 										product_cost, retail])
-		self.builder.get_object('label3').set_label(str(self.product_count))
-		self.builder.get_object('label5').set_label('${:,.2f}'.format(self.retail))
-		self.builder.get_object('label7').set_label('${:,.2f}'.format(self.cost))
+
+	def calculate_totals(self):
+		product_count = 0
+		cost = Decimal()
+		retail = Decimal()
+		for row in self.inventory_store:
+			product_count += 1
+			cost += Decimal(row[4] * row[2])
+			retail += Decimal(row[5] * row[2])
+		self.builder.get_object('label3').set_label(str(product_count))
+		self.builder.get_object('label5').set_label('${:,.2f}'.format(retail))
+		self.builder.get_object('label7').set_label('${:,.2f}'.format(cost))
 
 
 
