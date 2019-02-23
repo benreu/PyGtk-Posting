467fc8ea11c41a607a69e06970839bfcef37d304
Reuben Rissler <pygtk.posting@gmail.com>
2019-02-23 13:14:53 -0500

    add treestore processing to export_to_pdf report generator

--- src/reports/export_to_pdf.py
+++ src/reports/export_to_pdf.py
@@ -27,6 +27,7 @@ UI_FILE = main.ui_directory + "/reports/export_to_pdf.ui"
 
 class ExportToPdfGUI(Gtk.Builder):
 	landscape = False
+	border_width = 25
 	def __init__ (self, treeview):
 
 		Gtk.Builder.__init__(self)
@@ -47,24 +48,10 @@ class ExportToPdfGUI(Gtk.Builder):
 		return True
 
 	def generate_pdf(self):
-		if type(self.store) is type(Gtk.TreeStore()):
-			self.create_from_treestore()
-		elif type(self.store) is type(Gtk.ListStore()):
-			self.create_from_liststore()
-		else: # should not be reached
-			raise Exception("Invalid treestore model!")
-		self.window.present()
-			
-	def create_from_treestore (self):
-		for i in self.store:
-			print (self.treeview.row_expanded(i.path))
-			
-	def create_from_liststore (self):
-		border = 25
-		columns = list()
+		self.columns = list() # a list that contains a list of column attributes
 		for column in self.treeview.get_columns():
 			if column.get_visible():
-				specs = list()
+				specs = list() # the list that holds the column attributes
 				cell_area = column.get_area()
 				renderer = cell_area.get_cells()[0]
 				specs.append(cell_area.attribute_get_column(renderer, "text"))
@@ -76,8 +63,11 @@ class ExportToPdfGUI(Gtk.Builder):
 					xalign = Pango.Alignment.LEFT
 				specs.append(xalign)
 				specs.append(column.get_width())
-				columns.append(specs)
-		pdf_width = self.treeview.get_allocated_width() + (border * 2)
+				is_expander = self.treeview.get_expander_column() == column
+				specs.append(is_expander)
+				self.columns.append(specs)
+		pdf_width = self.treeview.get_allocated_width() 
+		pdf_width += self.border_width * 2
 		if pdf_width > 792: # greater than 11 inches
 			pdf_height = 612 * (float(pdf_width) / 792) 
 			# scale pdf height according to width oversize
@@ -89,17 +79,104 @@ class ExportToPdfGUI(Gtk.Builder):
 		else: # less than 8.5 inches
 			pdf_width = 612
 			pdf_height = 792
-		rectangle = self.treeview.get_cell_area(0, None)
+		self.pdf_height = pdf_height
+		self.pdf_width = pdf_width
 		surface = cairo.PDFSurface("/tmp/pdf_export.pdf", pdf_width, pdf_height)
-		cr = cairo.Context(surface)
-		cr.set_source_rgb(0, 0, 0)
-		cr.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
-									cairo.FONT_WEIGHT_NORMAL)
-		cr.set_font_size(11)
-		y = border
+		self.cr = cairo.Context(surface)
+		self.cr.set_source_rgb(0, 0, 0)
+		self.cr.set_line_width(1)
+		rectangle = self.treeview.get_cell_area(0, None)
+		self.row_height = rectangle.height
+		if type(self.store) is type(Gtk.TreeStore()):
+			treeiter = self.store.get_iter_first()
+			indent = 0
+			# self.y is the current position of the cursor in the document
+			self.y = self.border_width 
+			self.line_axis = list() # a list that contains a list of axises
+			self.create_from_treerow(treeiter, indent)
+		elif type(self.store) is type(Gtk.ListStore()):
+			self.create_from_liststore()
+		else: # should not be reached
+			raise Exception("Invalid treestore model!")
+		surface.finish()
+		self.window.present()
+		with open("/tmp/pdf_export.pdf",'rb') as f:
+			self.bytes = f.read()
+		GLib.idle_add(self.load_pdf)
+			
+	def create_from_treerow (self, treeiter, indent):
+		"create_from_treerow calls itself for child nodes"
+		start_y = self.y
+		while treeiter != None:
+			x, end_y = self.show_row(treeiter, indent )
+			axis = list() # keep track of axises for purpose of page breaks
+			axis.append(x)
+			axis.append(start_y)
+			axis.append(end_y)
+			self.line_axis.append(axis) # we need current line axis for page breaks
+			if (self.y + self.border_width) > self.pdf_height:
+				self.render_page_break()
+			if self.store.iter_has_child(treeiter):
+				if self.treeview.row_expanded(self.store.get_path(treeiter)):
+					childiter = self.store.iter_children(treeiter)
+					# enter child node
+					self.create_from_treerow (childiter, indent + 20)
+					# exit child node and process any other parent nodes
+			axis = self.line_axis.pop(-1) # remove axis as we exit the child node (if any)
+			x, start_y = axis[0], axis[1] # we use the axis for completing lines
+			treeiter = self.store.iter_next(treeiter)
+		if x != self.border_width : # render a vertical line for all child nodes
+			self.cr.move_to(x, start_y - (self.row_height / 2))
+			self.cr.line_to(x, end_y - (self.row_height / 1.5))
+			self.cr.stroke()
+			
+	def show_row (self, treeiter, indent):
+		row = self.store[treeiter] # the row we are displaying
+		x = self.border_width + indent 
+		start_x = x
+		if indent != 0: # show the horizontal line in front of child nodes only
+			y = self.y + (self.row_height / 3)
+			self.cr.move_to(x, y)
+			self.cr.line_to(x + 10, y)
+			self.cr.stroke()
+		x += 10
+		for specs in self.columns:
+			layout = PangoCairo.create_layout(self.cr)
+			desc = Pango.font_description_from_string("Sans 8")
+			layout.set_font_description(desc)
+			text = str(row[specs[0]])
+			layout.set_text(text, len(text))
+			layout.set_ellipsize(specs[1])
+			layout.set_alignment(specs[2])
+			layout.set_width(specs[3] * Pango.SCALE) # column_width
+			layout.set_height(-1)
+			self.cr.move_to(x, self.y)
+			PangoCairo.show_layout(self.cr, layout)
+			if specs[4]: # expander column; align the rest of the columns
+				x -= indent 
+			x += specs[3] # move right by the width of the column
+		self.y += self.row_height
+		return start_x, self.y
+
+	def render_page_break (self):
+		"completes vertical lines before finishing the page "
+		"and then restarts the lines on the new page"
+		for axis in self.line_axis:
+			x, start_y = axis[0], axis[1]
+			if x == self.border_width:
+				continue
+			self.cr.move_to(x, start_y - (self.row_height / 3))
+			self.cr.line_to(x, self.y + self.row_height ) # end of page
+			self.cr.stroke() # paint the line
+			axis[1] = 0 # line now starts at beginning of page
+		self.cr.show_page() # show existing page and start a new one
+		self.y = self.border_width # set the cursor y position to the start
+		
+	def create_from_liststore (self):
+		y = self.border_width
 		for index, row in enumerate(self.store):
-			x = border
-			for specs in columns:
+			x = self.border_width
+			for specs in self.columns:
 				layout = PangoCairo.create_layout(cr)
 				desc = Pango.font_description_from_string("Sans 9")
 				layout.set_font_description(desc)
@@ -114,19 +191,15 @@ class ExportToPdfGUI(Gtk.Builder):
 				PangoCairo.show_layout(cr, layout)
 				x += specs[3] + 10
 			y += rectangle.height 
-			if (y + border) > pdf_height:
+			if (y + self.border_width) > pdf_height:
 				cr.show_page()
-				y = border
-		surface.finish()
-		with open("/tmp/pdf_export.pdf",'rb') as f:
-			self.bytes = f.read()
-		GLib.idle_add(self.load_pdf)
+				y = self.border_width
 
 	def print_pdf_clicked (self, button):
 		import printing
 		print_op = printing.Setup()
 		pdf_data = Gio.MemoryInputStream.new_from_bytes(GLib.Bytes(self.bytes))
-		print_op.set_print_bytes(pdf_data)
+		print_op.set_bytes_to_print(pdf_data)
 		if self.landscape:
 			setup = Gtk.PageSetup()
 			setup.set_orientation(Gtk.PageOrientation.LANDSCAPE)
