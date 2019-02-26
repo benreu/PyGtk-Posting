6759fb9543c23b07e9368f8d26bc4549789c1167
Reuben Rissler <pygtk.posting@gmail.com>
2019-02-25 22:48:08 -0500

    in contact_history, add products tab add report hub button subclass Gtk Builder

--- src/reports/contact_history.py
+++ src/reports/contact_history.py
@@ -24,16 +24,16 @@ import main
 
 UI_FILE = main.ui_directory + "/reports/contact_history.ui"
 
-class ContactHistoryGUI:
+class ContactHistoryGUI (Gtk.Builder):
 	def __init__(self, main):
 
 		self.search_iter = 0
 		
-		self.builder = Gtk.Builder()
-		self.builder.add_from_file(UI_FILE)
-		self.builder.connect_signals(self)
+		Gtk.Builder.__init__(self)
+		self.add_from_file(UI_FILE)
+		self.connect_signals(self)
 
-		contact_completion = self.builder.get_object('contact_completion')
+		contact_completion = self.get_object('contact_completion')
 		contact_completion.set_match_func(self.contact_match_func)
 
 		self.main = main
@@ -42,7 +42,7 @@ class ContactHistoryGUI:
 
 		self.invoice_history = None
 
-		self.contact_store = self.builder.get_object('contact_store')
+		self.contact_store = self.get_object('contact_store')
 		self.cursor.execute("SELECT id::text, name, ext_name FROM contacts "
 							"WHERE deleted = False ORDER BY name")
 		for row in self.cursor.fetchall():
@@ -51,23 +51,23 @@ class ContactHistoryGUI:
 			ext_name = row[2]
 			self.contact_store.append([id_ , name, ext_name])
 
-		amount_column = self.builder.get_object ('treeviewcolumn22')
-		amount_renderer = self.builder.get_object ('cellrenderertext25')
+		amount_column = self.get_object ('treeviewcolumn22')
+		amount_renderer = self.get_object ('cellrenderertext25')
 		amount_column.set_cell_data_func(amount_renderer, self.amount_cell_func, 3)
 
-		amount_column = self.builder.get_object ('treeviewcolumn15')
-		amount_renderer = self.builder.get_object ('cellrenderertext18')
+		amount_column = self.get_object ('treeviewcolumn15')
+		amount_renderer = self.get_object ('cellrenderertext18')
 		amount_column.set_cell_data_func(amount_renderer, self.amount_cell_func, 4)
 
-		amount_column = self.builder.get_object ('treeviewcolumn10')
-		amount_renderer = self.builder.get_object ('cellrenderertext11')
+		amount_column = self.get_object ('treeviewcolumn10')
+		amount_renderer = self.get_object ('cellrenderertext11')
 		amount_column.set_cell_data_func(amount_renderer, self.amount_cell_func, 3)
 
-		amount_column = self.builder.get_object ('treeviewcolumn5')
-		amount_renderer = self.builder.get_object ('cellrenderertext5')
+		amount_column = self.get_object ('treeviewcolumn5')
+		amount_renderer = self.get_object ('cellrenderertext5')
 		amount_column.set_cell_data_func(amount_renderer, self.amount_cell_func, 5)
 		
-		self.window = self.builder.get_object('window1')
+		self.window = self.get_object('window1')
 		self.window.show_all()
 
 	def close_transaction_window (self, window, event):
@@ -93,17 +93,17 @@ class ContactHistoryGUI:
 			f.close()
 
 	def invoice_treeview_button_release_event (self, treeview, event):
-		selection = self.builder.get_object('treeview-selection4')
+		selection = self.get_object('treeview-selection4')
 		model, path = selection.get_selected_rows()
 		if path == []:
 			return
 		if event.button == 3:
-			menu = self.builder.get_object('invoice_menu')
+			menu = self.get_object('invoice_menu')
 			menu.popup(None, None, None, None, event.button, event.time)
 			menu.show_all()
 
 	def invoice_history_activated (self, menuitem):
-		selection = self.builder.get_object('treeview-selection4')
+		selection = self.get_object('treeview-selection4')
 		model, path = selection.get_selected_rows()
 		invoice_id = model[path][0]
 		if not self.invoice_history or self.invoice_history.exists == False:
@@ -138,16 +138,42 @@ class ContactHistoryGUI:
 		self.contact_id = model[iter][0]
 		self.populate_contact_stores ()
 
+	def report_hub_clicked (self, button):
+		tab_num = self.get_object('notebook1').get_current_page()
+		scrolled_window = self.get_object('notebook1').get_nth_page(tab_num)
+		treeview = scrolled_window.get_child()
+		from reports import report_hub
+		report_hub.ReportHubGUI(treeview)
+
+	def product_selection_changed (self, selection):
+		rows = len(selection.get_selected_rows()[1])
+		if rows > 1:
+			menu = self.get_object('product_menu')
+			menu.show_all()
+			menu.popup_at_pointer()
+
+	def hide_rows_activated (self, menuitem):
+		"we need to convert paths to iters, because paths are wrong after "
+		"deleting a row in the model "
+		model, paths = self.get_object('product_selection').get_selected_rows()
+		iter_list = list()
+		if paths != []:
+			for path in paths:
+				iter_list.append(model.get_iter(path))
+			for iter_ in iter_list:
+				model.remove(iter_)
+
 	def populate_contact_stores (self):
 		self.populate_contact_invoices()
 		self.populate_contact_payments()
+		self.populate_product_store ()
 		self.populate_resources ()
 		self.populate_contact_statements ()
 		self.populate_warranty_store ()
 		self.populate_incoming_invoice_store ()
 
 	def populate_incoming_invoice_store (self):
-		incoming_invoice_store = self.builder.get_object('incoming_invoice_store')
+		incoming_invoice_store = self.get_object('incoming_invoice_store')
 		incoming_invoice_store.clear()
 		count = 0
 		self.cursor.execute("SELECT id, date_created::text, "
@@ -160,13 +186,13 @@ class ContactHistoryGUI:
 			count += 1
 			incoming_invoice_store.append(row)
 		if count == 0:
-			self.builder.get_object('label9').set_label('Incoming invoices')
+			self.get_object('label9').set_label('Incoming invoices')
 		else:
 			label = "<span weight='bold'>Incoming invoices (%s)</span>" % count
-			self.builder.get_object('label9').set_markup(label)
+			self.get_object('label9').set_markup(label)
 
 	def populate_warranty_store (self):
-		warranty_store = self.builder.get_object('warranty_store')
+		warranty_store = self.get_object('warranty_store')
 		warranty_store.clear()
 		count = 0
 		self.cursor.execute("SELECT snh.id, p.name, sn.serial_number, "
@@ -183,13 +209,79 @@ class ContactHistoryGUI:
 			count += 1
 			warranty_store.append(row)
 		if count == 0:
-			self.builder.get_object('label8').set_label('Warranty')
+			self.get_object('label8').set_label('Warranty')
 		else:
 			label = "<span weight='bold'>Warranty (%s)</span>" % count
-			self.builder.get_object('label8').set_markup(label)
+			self.get_object('label8').set_markup(label)
+
+	def populate_product_store (self):
+		product_store = self.get_object('product_store')
+		product_store.clear()
+		count = 0
+		c_id = self.contact_id
+		c = self.db.cursor()
+		c.execute("SELECT * FROM "
+					"(SELECT SUM(qty), "
+					"SUM(qty)::text, "
+					"p.id, "
+					"p.name AS name, "
+					"remark, "
+					"'Invoice' "
+					"FROM products AS p "
+					"JOIN invoice_items AS ili ON ili.product_id = p.id "
+					"JOIN invoices AS i ON i.id = ili.invoice_id "
+					"WHERE customer_id = %s "
+					"GROUP BY p.id, p.name, remark) inv "
+				"UNION "
+					"(SELECT SUM(qty), "
+					"SUM(qty)::text, "
+					"p.id, "
+					"p.name AS name, "
+					"remark, "
+					"'Purchase order' "
+					"FROM products AS p "
+					"JOIN purchase_order_line_items AS poli ON poli.product_id = p.id "
+					"JOIN purchase_orders AS po ON po.id = poli.purchase_order_id "
+					"WHERE vendor_id = %s "
+					"GROUP BY p.id, p.name, remark) "
+				"UNION "
+					"(SELECT SUM(qty), "
+					"SUM(qty)::text, "
+					"p.id, "
+					"p.name AS name, "
+					"remark, "
+					"'Documents' "
+					"FROM products AS p "
+					"JOIN document_lines AS dl ON dl.product_id = p.id "
+					"JOIN documents AS d ON d.id = dl.document_id "
+					"WHERE contact_id = %s "
+					"GROUP BY p.id, p.name, remark) "
+				"UNION "
+					"(SELECT SUM(qty), "
+					"SUM(qty)::text, "
+					"p.id, "
+					"p.name AS name, "
+					"remark, "
+					"'Job Sheet' "
+					"FROM products AS p "
+					"JOIN job_sheet_line_items AS jsli ON jsli.product_id = p.id "
+					"JOIN job_sheets AS js ON js.id = jsli.job_sheet_id "
+					"WHERE contact_id = %s "
+					"GROUP BY p.id, p.name, remark) "
+					"ORDER BY name", 
+					(c_id, c_id, c_id, c_id))
+		for row in c.fetchall():
+			count += 1
+			product_store.append(row)
+		if count == 0:
+			self.get_object('label10').set_label('Products')
+		else:
+			label = "<span weight='bold'>Products (%s)</span>" % count
+			self.get_object('label10').set_markup(label)
+		c.close()
 
 	def populate_contact_statements (self):
-		statement_store = self.builder.get_object('statements_store')
+		statement_store = self.get_object('statements_store')
 		statement_store.clear()
 		count = 0
 		self.cursor.execute("SELECT id, date_inserted::text, "
@@ -204,13 +296,13 @@ class ContactHistoryGUI:
 			count += 1
 			statement_store.append(row)
 		if count == 0:
-			self.builder.get_object('label7').set_label('Statements')
+			self.get_object('label7').set_label('Statements')
 		else:
 			label = "<span weight='bold'>Statements (%s)</span>" % count
-			self.builder.get_object('label7').set_markup(label)
+			self.get_object('label7').set_markup(label)
 
 	def populate_resources (self):
-		resource_store = self.builder.get_object('resource_store')
+		resource_store = self.get_object('resource_store')
 		resource_store.clear()
 		count = 0
 		self.cursor.execute("SELECT r.id, subject, dated_for::text, "
@@ -236,13 +328,13 @@ class ContactHistoryGUI:
 			resource_store.append([row_id, subject, dated_for, 
 									date_formatted, notes, tag_name, rgba])
 		if count == 0:
-			self.builder.get_object('label4').set_label('Resources')
+			self.get_object('label4').set_label('Resources')
 		else:
 			label = "<span weight='bold'>Resources (%s)</span>" % count
-			self.builder.get_object('label4').set_markup(label)
+			self.get_object('label4').set_markup(label)
 
 	def populate_contact_payments (self):
-		payment_store = self.builder.get_object('payments_store')
+		payment_store = self.get_object('payments_store')
 		payment_store.clear()
 		count = 0
 		self.cursor.execute("SELECT id, date_inserted::text, "
@@ -255,13 +347,13 @@ class ContactHistoryGUI:
 			count += 1
 			payment_store.append(row)
 		if count == 0:
-			self.builder.get_object('label3').set_label('Payments received')
+			self.get_object('label3').set_label('Payments received')
 		else:
 			label = "<span weight='bold'>Payments received (%s)</span>" % count
-			self.builder.get_object('label3').set_markup(label)
+			self.get_object('label3').set_markup(label)
 
 	def populate_contact_invoices (self):
-		invoice_store = self.builder.get_object('invoice_store')
+		invoice_store = self.get_object('invoice_store')
 		invoice_store.clear()
 		count = 0
 		self.cursor.execute("SELECT i.id, dated_for::text, "
@@ -277,10 +369,10 @@ class ContactHistoryGUI:
 			count += 1
 			invoice_store.append(row)
 		if count == 0:
-			self.builder.get_object('label2').set_label('Invoices')
+			self.get_object('label2').set_label('Invoices')
 		else:
 			label = "<span weight='bold'>Invoices (%s)</span>" % count
-			self.builder.get_object('label2').set_markup(label)
+			self.get_object('label2').set_markup(label)
 
 
 
