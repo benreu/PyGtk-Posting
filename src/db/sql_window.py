76fd0fb4385361cd1d06004b3b7b5e9687106f2d
Reuben Rissler <pygtk.posting@gmail.com>
2019-05-10 09:10:44 -0400

    remove sql_window db variable (leftover from db import reconstruction)

--- src/db/sql_window.py
+++ src/db/sql_window.py
@@ -23,13 +23,13 @@ import constants
 UI_FILE = constants.ui_directory + "/db/sql_window.ui"
 
 class SQLWindowGUI :
-	def __init__(self, db):
+	def __init__(self):
 		
 		self.builder = Gtk.Builder()
 		GObject.type_register(GtkSource.View)
 		self.builder.add_from_file(UI_FILE)
 		self.builder.connect_signals(self)
-		self.db = db
+		self.db = main.db
 
 		language_manager = GtkSource.LanguageManager()
 		self.source_view = self.builder.get_object('gtksourceview1')
