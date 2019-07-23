8176c69c3bb357a6878cc11523f4ea18afef49ec
Reuben Rissler <pygtk.posting@gmail.com>
2019-07-23 13:28:45 -0400

    in job_types, update main.db to constants.db refactor code

--- src/job_types.py
+++ src/job_types.py
@@ -15,8 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk, GdkPixbuf, Gdk
-import os, sys
+from gi.repository import Gtk
 import constants
 
 UI_FILE = constants.ui_directory + "/job_types.ui"
@@ -29,22 +28,21 @@ class GUI (Gtk.Builder):
 		self.add_from_file(UI_FILE)
 		self.connect_signals(self)
 
-		self.db = main.db
+		self.db = constants.db
 		self.cursor = self.db.cursor()
 
 		self.job_store = self.get_object('job_type_store')
 		self.job_treeview_populate ()
 
-		self.id_ = 0
+		self.job_id = 0
 		
 		self.window = self.get_object('window1')
 		self.window.show_all()
 
 	def row_activate(self, treeview, path, treeviewcolumn):
-		treeiter = self.job_store.get_iter(path)
-		self.id_ = self.job_store.get_value(treeiter, 0)
+		self.job_id = self.job_store[path][0]
 		self.cursor.execute("SELECT name FROM job_types "
-							"WHERE id = %s", (self.id_, ) )
+							"WHERE id = %s", (self.job_id, ) )
 		for row in self.cursor.fetchall():
 			name = row[0]
 			self.get_object('entry1').set_text(name)
@@ -57,13 +55,13 @@ class GUI (Gtk.Builder):
 
 	def save(self, widget):
 		job = widget.get_text()
-		if self.id_ == 0:
+		if self.job_id == 0:
 			self.cursor.execute("INSERT INTO job_types (name) "
 								"VALUES (%s)", (job,))
 		else:
 			self.cursor.execute("UPDATE job_types "
 								"SET name = %s "
-								"WHERE id = %s", (job,self.id_))
+								"WHERE id = %s", (job, self.job_id))
 		self.db.commit()
 		self.job_treeview_populate ()
 
@@ -71,16 +69,15 @@ class GUI (Gtk.Builder):
 		selection = self.get_object("treeview-selection1")
 		store, path = selection.get_selected_rows ()
 		if path != []:
-			treeiter = self.job_store.get_iter(path)
-			id_ = self.job_store.get_value(treeiter, 0)
+			job_id = self.job_store[path][0]
 			self.cursor.execute("UPDATE job_types SET deleted = True "
-								"WHERE id = %s", (id_ ,))
+								"WHERE id = %s", (job_id ,))
 			self.db.commit()
 			self.job_treeview_populate ()
 
 	def new(self, widget):
 		widget.set_text("New job type")
-		self.id_ = 0
+		self.job_id = 0
 
 
 
