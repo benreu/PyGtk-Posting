7163123e9e91e20726a36baac0eb161efa6bd4e6
Reuben Rissler <pygtk.posting@gmail.com>
2019-12-05 18:24:22 -0500

    in update_db_minor.sql, specifically check for existing foreign keys

--- src/db/update_db_minor.sql
+++ src/db/update_db_minor.sql
@@ -41,7 +41,20 @@ ALTER TABLE resource_tags ALTER COLUMN green SET NOT NULL;
 ALTER TABLE resource_tags ALTER COLUMN alpha SET NOT NULL;
 --INSERT INTO resource_tags (tag, red, green, blue, alpha, finished) VALUES ('To do', 0, 0, 0, 1, False);
 
-ALTER TABLE time_clock_projects ADD COLUMN IF NOT EXISTS resource_id bigint UNIQUE REFERENCES resources ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE time_clock_projects ADD COLUMN IF NOT EXISTS resource_id bigint UNIQUE;
+DO $$
+BEGIN
+	IF NOT EXISTS (SELECT constraint_schema, constraint_name 
+		FROM information_schema.constraint_column_usage 
+		WHERE constraint_schema = 'public'
+		AND constraint_name = 'shipping_info_incoming_invoice_id_fkey' )
+	THEN
+		ALTER TABLE public.shipping_info
+		ADD CONSTRAINT shipping_info_incoming_invoice_id_fkey FOREIGN KEY (incoming_invoice_id)
+		REFERENCES public.incoming_invoices (id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE RESTRICT;
+	END IF;
+END$$; 
 -- version 0.4.3
 ALTER TABLE credit_memo_items ADD COLUMN IF NOT EXISTS ext_price numeric (12, 2) DEFAULT 0.00;
 UPDATE credit_memo_items SET ext_price = (qty * price) WHERE ext_price IS NULL;
@@ -83,7 +96,20 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 --version 0.5.1
-ALTER TABLE credit_memo_items ADD COLUMN IF NOT EXISTS gl_entry_id bigint REFERENCES gl_entries ON UPDATE RESTRICT ON DELETE RESTRICT; 
+ALTER TABLE credit_memo_items ADD COLUMN IF NOT EXISTS gl_entry_id bigint;
+DO $$
+BEGIN
+	IF NOT EXISTS (SELECT constraint_schema, constraint_name 
+		FROM information_schema.constraint_column_usage 
+		WHERE constraint_schema = 'public'
+		AND constraint_name = 'credit_memo_items_gl_entry_id_fkey' )
+	THEN
+		ALTER TABLE public.credit_memo_items
+		ADD CONSTRAINT credit_memo_items_gl_entry_id_fkey FOREIGN KEY (gl_entry_id)
+		REFERENCES public.gl_entries (id) MATCH SIMPLE
+		ON UPDATE RESTRICT ON DELETE RESTRICT;
+	END IF;
+END$$; 
 --version 0.5.2
 CREATE TABLE IF NOT EXISTS public.shipping_info (id serial PRIMARY KEY, tracking_number varchar NOT NULL UNIQUE, reason varchar, invoice_id bigint UNIQUE REFERENCES invoices ON DELETE RESTRICT ON UPDATE RESTRICT, CONSTRAINT shipment_reason_not_null CHECK (reason IS NOT NULL OR invoice_id IS NOT NULL));
 --version 0.5.3
@@ -132,13 +158,39 @@ ALTER TABLE log.products ALTER COLUMN barcode DROP DEFAULT;
 CREATE TABLE IF NOT EXISTS public.budgets (id serial primary key, name varchar NOT NULL, fiscal_id bigint NOT NULL REFERENCES fiscal_years ON DELETE RESTRICT, total numeric(12,2) NOT NULL, date_created date NOT NULL DEFAULT now(), active boolean NOT NULL DEFAULT TRUE);
 CREATE TABLE IF NOT EXISTS public.budget_amounts (id serial primary key, budget_id bigint NOT NULL REFERENCES budgets ON DELETE RESTRICT ON UPDATE RESTRICT, name varchar NOT NULL, amount numeric(12,2) NOT NULL, account bigint NOT NULL REFERENCES gl_accounts ON DELETE RESTRICT ON UPDATE CASCADE, date_created date NOT NULL DEFAULT now());
 --version 0.5.7
-ALTER TABLE incoming_invoices ADD COLUMN IF NOT EXISTS gl_entry_id bigint REFERENCES gl_entries ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE incoming_invoices ADD COLUMN IF NOT EXISTS gl_entry_id bigint;
+DO $$
+BEGIN
+	IF NOT EXISTS (SELECT constraint_schema, constraint_name 
+		FROM information_schema.constraint_column_usage 
+		WHERE constraint_schema = 'public'
+		AND constraint_name = 'incoming_invoices_gl_entry_id_fkey' )
+	THEN
+		ALTER TABLE public.incoming_invoices
+		ADD CONSTRAINT incoming_invoices_gl_entry_id_fkey FOREIGN KEY (gl_entry_id)
+		REFERENCES public.gl_entries (id) MATCH SIMPLE
+		ON UPDATE RESTRICT ON DELETE RESTRICT;
+	END IF;
+END$$; 
 CREATE TABLE IF NOT EXISTS incoming_invoices_gl_entry_expenses_ids (id bigserial PRIMARY KEY, gl_entry_expense_id bigint NOT NULL REFERENCES gl_entries ON DELETE RESTRICT ON UPDATE RESTRICT, incoming_invoices_id bigint NOT NULL REFERENCES incoming_invoices ON DELETE RESTRICT ON UPDATE RESTRICT);
 --version 0.5.8
 ALTER TABLE public.purchase_order_line_items DROP CONSTRAINT IF EXISTS purchase_order_line_items_expense_account_fkey;
 ALTER TABLE public.purchase_order_line_items ADD CONSTRAINT purchase_order_line_items_expense_account_fkey FOREIGN KEY (expense_account) REFERENCES public.gl_accounts ("number") MATCH SIMPLE ON UPDATE CASCADE ON DELETE RESTRICT;
 --version 0.5.9
-ALTER TABLE incoming_invoices ADD COLUMN IF NOT EXISTS gl_transaction_id bigint REFERENCES gl_transactions ON DELETE RESTRICT;
+ALTER TABLE incoming_invoices ADD COLUMN IF NOT EXISTS gl_transaction_id bigint;
+DO $$
+BEGIN
+	IF NOT EXISTS (SELECT constraint_schema, constraint_name 
+		FROM information_schema.constraint_column_usage 
+		WHERE constraint_schema = 'public'
+		AND constraint_name = 'incoming_invoices_gl_transaction_id_fkey' )
+	THEN
+		ALTER TABLE public.incoming_invoices
+		ADD CONSTRAINT incoming_invoices_gl_transaction_id_fkey FOREIGN KEY (gl_transaction_id)
+		REFERENCES public.gl_transactions (id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE RESTRICT;
+	END IF;
+END$$; 
 UPDATE incoming_invoices SET gl_transaction_id = ge.gl_transaction_id FROM (SELECT id, gl_transaction_id FROM gl_entries) AS ge WHERE incoming_invoices.gl_entry_id = ge.id AND incoming_invoices.gl_transaction_id IS NULL;
 UPDATE incoming_invoices SET gl_transaction_id = ge.gl_transaction_id FROM (SELECT date_inserted, amount, gl_transaction_id FROM gl_entries) AS ge WHERE incoming_invoices.amount = ge.amount AND incoming_invoices.gl_transaction_id IS NULL AND incoming_invoices.date_created = ge.date_inserted ;
 --version 0.5.10
@@ -156,10 +208,36 @@ ALTER TABLE public.document_items ALTER COLUMN min SET DEFAULT 0.00;
 ALTER TABLE public.document_items ALTER COLUMN max SET DEFAULT 100.00;
 ALTER TABLE public.document_items ALTER COLUMN qty SET DEFAULT 1.00;
 --version 0.5.12
-ALTER TABLE shipping_info ADD COLUMN IF NOT EXISTS incoming_invoice_id bigint REFERENCES incoming_invoices ON DELETE RESTRICT;
+ALTER TABLE shipping_info ADD COLUMN IF NOT EXISTS incoming_invoice_id bigint;
+DO $$
+BEGIN
+	IF NOT EXISTS (SELECT constraint_schema, constraint_name 
+		FROM information_schema.constraint_column_usage 
+		WHERE constraint_schema = 'public'
+		AND constraint_name = 'shipping_info_incoming_invoice_id_fkey' )
+	THEN
+		ALTER TABLE public.shipping_info
+		ADD CONSTRAINT shipping_info_incoming_invoice_id_fkey FOREIGN KEY (incoming_invoice_id)
+		REFERENCES public.incoming_invoices (id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE RESTRICT;
+	END IF;
+END$$; 
 --version 0.5.13
 ALTER TABLE shipping_info ADD COLUMN IF NOT EXISTS date_shipped date DEFAULT now();
-ALTER TABLE shipping_info ADD COLUMN IF NOT EXISTS contact_id bigint REFERENCES contacts ON DELETE RESTRICT;
+ALTER TABLE shipping_info ADD COLUMN IF NOT EXISTS contact_id bigint;
+DO $$
+BEGIN
+	IF NOT EXISTS (SELECT constraint_schema, constraint_name 
+		FROM information_schema.constraint_column_usage 
+		WHERE constraint_schema = 'public'
+		AND constraint_name = 'shipping_info_contact_id_fkey' )
+	THEN
+		ALTER TABLE public.shipping_info
+		ADD CONSTRAINT shipping_info_contact_id_fkey FOREIGN KEY (contact_id)
+		REFERENCES public.contacts (id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE RESTRICT;
+	END IF;
+END$$; 
 UPDATE shipping_info SET contact_id = c_join.contact_id FROM (SELECT co.id AS contact_id, i.id AS invoice_id FROM contacts AS co JOIN invoices AS i ON i.customer_id = co.id) AS c_join WHERE c_join.invoice_id = shipping_info.invoice_id;
 UPDATE shipping_info SET date_shipped = c_join.dated_for FROM (SELECT co.id AS contact_id, i.dated_for AS dated_for, i.id AS invoice_id FROM contacts AS co JOIN invoices AS i ON i.customer_id = co.id) AS c_join WHERE c_join.invoice_id = shipping_info.invoice_id;
 ALTER TABLE shipping_info ALTER COLUMN contact_id SET NOT NULL;
