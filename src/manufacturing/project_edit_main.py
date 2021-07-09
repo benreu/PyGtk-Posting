8f8833ff7d0085731c28904b09232a081612ef55
Reuben Rissler <pygtk.posting@gmail.com>
2021-07-09 06:48:06 -0400

    add version selection to manufacturing project_edit_main window

--- src/manufacturing/project_edit_main.py
+++ src/manufacturing/project_edit_main.py
@@ -33,15 +33,22 @@ class ProjectEditGUI(Gtk.Builder):
 		self.product_store = self.get_object('product_store')
 		product_completion = self.get_object('product_completion')
 		product_completion.set_match_func(self.product_match_func)
+		self.version_store = self.get_object('version_store')
+		version_completion = self.get_object('version_completion')
+		version_completion.set_match_func(self.version_match_func)
 		self.parent_class = parent_class
 		self.project_id = project_id
 		self.populate_stores ()
 		self.product_id = None
+		self.version_id = None
 		self.window = self.get_object('window')
 		self.window.show_all()
 		if project_id != None:
 			self.load_project()
-		
+	
+	def widget_focus_in_event (self, widget, event):
+		GLib.idle_add(widget.select_region, 0, -1)
+	
 	def populate_stores(self):
 		cursor = DB.cursor()
 		self.product_store.clear()
@@ -67,12 +74,37 @@ class ProjectEditGUI(Gtk.Builder):
 	def product_combo_changed(self, combo):
 		product_id = combo.get_active_id()
 		if product_id != None:
+			c = DB.cursor()
 			self.product_id = product_id
-			self.get_object('save_button').set_sensitive(True)
+			c = DB.cursor()
+			c.execute("SELECT id::text, version_name "
+						"FROM product_assembly_versions "
+						"WHERE product_id = %s AND active = True",
+						(self.product_id,))
+			for row in c.fetchall():
+				self.version_store.append(row)
+			c.close()
+
+	def version_match_func(self, completion, key, iter_):
+		split_search_text = key.split()
+		for text in split_search_text:
+			if text not in self.version_store[iter_][1].lower():
+				return False
+		return True
+
+	def version_match_selected(self, completion, model, iter_):
+		version_id = model[iter_][0]
+		self.get_object('version_combo').set_active_id(version_id)
+		return True
+
+	def version_combo_changed (self, combo):
+		version_id = combo.get_active_id()
+		if version_id != None:
+			self.version_id = version_id
 			self.generate_project_description ()
 
 	def qty_spinbutton_changed (self, spinbutton):
-		if not self.product_id:
+		if not self.product_id or not self.version_id:
 			return
 		self.generate_project_description ()
 
@@ -86,11 +118,16 @@ class ProjectEditGUI(Gtk.Builder):
 		manufacturing_name_string = "Manufacturing : %s [%s]" % (p_name, qty)
 		self.get_object('description_entry').set_text(manufacturing_name_string)
 		cursor.close()
+		self.get_object('save_button').set_sensitive(True)
 
 	def load_project (self):
 		cursor = DB.cursor()
 		try:
-			cursor.execute("SELECT product_id::text, qty, name, batch_notes "
+			cursor.execute("SELECT product_id::text, "
+							"qty, "
+							"name, "
+							"batch_notes, "
+							"version_id::text "
 							"FROM manufacturing_projects WHERE id = %s "
 							"FOR UPDATE NOWAIT",
 							(self.project_id, ))
@@ -106,6 +143,7 @@ class ProjectEditGUI(Gtk.Builder):
 			self.get_object('units_spinbutton').set_value(row[1])
 			self.get_object('description_entry').set_text(row[2])
 			self.get_object('batch_notes_buffer').set_text(row[3])
+			self.get_object('version_combo').set_active_id(row[4])
 		self.get_object('product_combo').set_sensitive(False)
 		cursor.close()
 
@@ -122,7 +160,7 @@ class ProjectEditGUI(Gtk.Builder):
 		self.window.set_urgency_hint(False)
 
 	def save_clicked (self, button):
-		cursor = DB.cursor()
+		c = DB.cursor()
 		product_id = self.get_object('product_combo').get_active_id()
 		name = self.get_object('description_entry').get_text()
 		qty = self.get_object('units_spinbutton').get_value()
@@ -132,27 +170,28 @@ class ProjectEditGUI(Gtk.Builder):
 		notes = buf.get_text(start_iter, end_iter, True)
 		if self.project_id == None:
 			time_clock_id = self.get_time_clock_id (name)
-			cursor.execute("INSERT INTO manufacturing_projects "
-							"(product_id, name, qty, time_clock_projects_id, "
-							"batch_notes, active) VALUES "
-							"(%s, %s, %s, %s, %s, True)", 
-							(product_id, name, qty, time_clock_id, notes))
+			c.execute("INSERT INTO manufacturing_projects "
+						"(product_id, name, qty, time_clock_projects_id, "
+						"batch_notes, active, version_id) VALUES "
+						"(%s, %s, %s, %s, %s, True, %s)", 
+						(product_id, name, qty, time_clock_id, 
+						notes, self.version_id))
 		else:
-			cursor.execute("UPDATE manufacturing_projects SET "
-							"(name, qty, batch_notes) = "
-							"(%s, %s, %s) WHERE id = %s "
-							"RETURNING time_clock_projects_id", 
-							(name, qty, notes, self.project_id))
+			c.execute("UPDATE manufacturing_projects SET "
+						"(name, qty, batch_notes, version_id) = "
+						"(%s, %s, %s, %s) WHERE id = %s "
+						"RETURNING time_clock_projects_id", 
+						(name, qty, notes, self.version_id, self.project_id))
 			active = self.get_object('time_clock_checkbutton').get_active()
 			for row in cursor.fetchall():
 				time_clock_projects_id = row[0]
-				cursor.execute("UPDATE time_clock_projects "
-								"SET (name, active, stop_date) = "
-								"(%s, %s, CURRENT_DATE) "
-								"WHERE id = %s",
-								(name, active, time_clock_projects_id))
+				c.execute("UPDATE time_clock_projects "
+							"SET (name, active, stop_date) = "
+							"(%s, %s, CURRENT_DATE) "
+							"WHERE id = %s",
+							(name, active, time_clock_projects_id))
 		DB.commit()
-		cursor.close()
+		c.close()
 		self.window.destroy ()
 		self.parent_class.populate_projects()
 
